c964b6909edddfcb46a8f1240edab3920acf09d6
Vincent Fischer <vincent.i.fischer@gmail.com>
2023-03-04 23:03:40 +0100

    feat: add active nav item

---
+++ src/components/navigation/NavItems.ts
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+  { title: "Home", path: "" },
+  { title: "Skills", path: "skills" },
+  { title: "Work", path: "work" },
+  { title: "Contact", path: "contact" }
+] as const
