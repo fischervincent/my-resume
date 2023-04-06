3ca9ff64eb01103154b3e45cb21b9c80c9f21a92
Vincent Fischer <vincent.i.fischer@gmail.com>
2023-04-06 17:48:41 +0200

    add My Value page

--- src/components/navigation/NavItems.ts
+++ src/components/navigation/NavItems.ts
@@ -2,5 +2,6 @@ export const NAV_ITEMS = [
   { title: "Introduction", path: "" },
   { title: "Skills", path: "skills" },
   { title: "Work", path: "work" },
+  { title: "My Values", path: "values" },
   { title: "Contact", path: "contact" }
 ] as const
