9cafef128176e06bbadf522eac6177bec1ed0f6d
Vincent Fischer <vincent.i.fischer@gmail.com>
2024-10-02 22:15:45 +0200

    feat: remove WIP from work experiences

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -38,25 +38,6 @@ export const Projects = () => {
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="alert alert-info">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="stroke-current shrink-0 w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg>
-        <span className="flex-1">
-          This page is still under construction. The information here is not
-          valid.
-        </span>
-      </div>
       <div className="max-w-[500px]">
         <h5 className="text-sm font-extralight">
           Filter by skills, roles or methodologies
